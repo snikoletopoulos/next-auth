c0063dd4b3b29131f57962b1f2fc7f21d0865fd3
Stavros <snikoletopoulos@hotmail.com>
2022-04-21 21:56:08 +0300

    Type safe everything

--- src/helpers/db.helpers.ts
+++ src/helpers/db.helpers.ts
@@ -2,10 +2,20 @@ import { MongoClient } from "mongodb";
 
 export const connectToDb = async () => {
 	try {
-		const client = MongoClient.connect(process.env.MONGO_URL ?? "");
+		const client = await MongoClient.connect(process.env.MONGO_URL ?? "");
+
+		if (!client) {
+			throw new Error("Could not connect to database");
+		}
 
 		return client;
 	} catch (error) {
-		
+		if (typeof error === "string") {
+			throw new Error(error);
+		} else if (error instanceof Error) {
+			throw new Error(error.message);
+		}
+
+		throw new Error("Something went wrong");
 	}
 };
--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -22,27 +22,37 @@ const handler: NextApiHandler<Response> = async (req, res) => {
 		return;
 	}
 
-	const client = await connectToDb();
-	const db = client.db();
+	try {
+		const client = await connectToDb();
+		const db = client.db();
 
-	const collection = db.collection("users");
+		const collection = db.collection("users");
 
-	const existingUser = await collection.findOne({ email });
+		const existingUser = await collection.findOne({ email });
 
-	if (existingUser) {
-		res.status(422).json({ message: "User already exists" });
+		if (existingUser) {
+			res.status(422).json({ message: "User already exists" });
+			client.close();
+			return;
+		}
+
+		const result = await collection.insertOne({
+			email,
+			password: await hashPassword(password),
+		});
+
+		res.status(201).json({ message: "User created" });
+
 		client.close();
-		return;
+	} catch (error) {
+		let message = "Something went wrong";
+		if (typeof error === "string") {
+			message = error;
+		} else if (error instanceof Error) {
+			message = error.message;
+		}
+		res.status(500).json({ message });
 	}
-
-	const result = await collection.insertOne({
-		email,
-		password: await hashPassword(password),
-	});
-
-	res.status(201).json({ message: "User created" });
-
-  client.close()
 };
 
 export default handler;
