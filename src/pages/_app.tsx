cc5a945a746376c9fc0a72c502678ec7506a0f07
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 00:32:39 +0300

    Wrap app inside a SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 
 import Layout from "components/layout/Layout";
+import { SessionProvider } from "next-auth/react";
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp: React.FC<AppProps> = ({
+	Component,
+	pageProps: { session, ...pageProps },
+}) => {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<SessionProvider session={session}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SessionProvider>
 	);
 };
 
