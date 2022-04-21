89be2d92da347b93cece8cff082e8ea8e0609a42
Stavros <snikoletopoulos@hotmail.com>
2022-04-21 21:57:26 +0300

    Add functionality to log in/ signup with client side validation

--- src/components/auth/AuthForm/AuthForm.tsx
+++ src/components/auth/AuthForm/AuthForm.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import styles from "./AuthForm.module.css";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { createUser } from "services/auth.services";
+
 const AuthForm: React.FC = () => {
 	const [isLogin, setIsLogin] = useState(true);
 
@@ -8,17 +12,53 @@ const AuthForm: React.FC = () => {
 		setIsLogin(prevState => !prevState);
 	};
 
+	const authForm = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		validationSchema: Yup.object({
+			email: Yup.string().email("Invalid email address").required("Required"),
+			password: Yup.string()
+				.min(6, "Password must be at least 6 characters")
+				.required("Required"),
+		}),
+		onSubmit: async ({ email, password }) => {
+			if (isLogin) {
+				console.log("Login");
+			} else {
+				try {
+					const result = await createUser(email, password);
+
+					console.log(result);
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		},
+	});
+
 	return (
 		<section className={styles.auth}>
 			<h1>{isLogin ? "Login" : "Sign Up"}</h1>
-			<form>
+			<form onSubmit={authForm.handleSubmit}>
 				<div className={styles.control}>
 					<label htmlFor="email">Your Email</label>
-					<input type="email" id="email" required />
+					<input
+						type="email"
+						id="email"
+						required
+						{...authForm.getFieldProps("email")}
+					/>
 				</div>
 				<div className={styles.control}>
 					<label htmlFor="password">Your Password</label>
-					<input type="password" id="password" required />
+					<input
+						type="password"
+						id="password"
+						required
+						{...authForm.getFieldProps("email")}
+					/>
 				</div>
 				<div className={styles.actions}>
 					<button>{isLogin ? "Login" : "Create Account"}</button>
