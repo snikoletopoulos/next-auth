c2cd015ddfa3cb1ece9455bcae3bedb5c14e034f
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 00:33:02 +0300

    Render navigation buttons based on authentication status

--- src/components/layout/MainNavigation/MainNavigation.tsx
+++ src/components/layout/MainNavigation/MainNavigation.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
-
 import styles from "./MainNavigation.module.css";
 
+import { useSession } from "next-auth/react";
+
 const MainNavigation: React.FC = () => {
+	const { data: session, status } = useSession();
+	console.log({ session, status });
 	return (
 		<header className={styles.header}>
 			<Link href="/">
@@ -12,15 +15,21 @@ const MainNavigation: React.FC = () => {
 			</Link>
 			<nav>
 				<ul>
-					<li>
-						<Link href="/auth">Login</Link>
-					</li>
-					<li>
-						<Link href="/profile">Profile</Link>
-					</li>
-					<li>
-						<button>Logout</button>
-					</li>
+					{status !== "authenticated" && (
+						<li>
+							<Link href="/auth">Login</Link>
+						</li>
+					)}
+					{status === "authenticated" && (
+						<>
+							<li>
+								<Link href="/profile">Profile</Link>
+							</li>
+							<li>
+								<button>Logout</button>
+							</li>
+						</>
+					)}
 				</ul>
 			</nav>
 		</header>
