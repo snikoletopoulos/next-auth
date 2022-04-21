768d780e8a07a359b295c739357d9e89f2138620
Stavros <snikoletopoulos@hotmail.com>
2022-04-21 22:58:23 +0300

    Clean code

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -2,6 +2,7 @@ import type { NextApiHandler } from "next";
 
 import { connectToDb } from "helpers/db.helpers";
 import { hashPassword } from "helpers/auth.helpers";
+import { MongoClient } from "mongodb";
 
 export interface Response {
 	message: string;
@@ -22,37 +23,39 @@ const handler: NextApiHandler<Response> = async (req, res) => {
 		return;
 	}
 
+	let client: MongoClient;
 	try {
-		const client = await connectToDb();
-		const db = client.db();
-
-		const collection = db.collection("users");
-
-		const existingUser = await collection.findOne({ email });
-
-		if (existingUser) {
-			res.status(422).json({ message: "User already exists" });
-			client.close();
-			return;
-		}
-
-		const result = await collection.insertOne({
-			email,
-			password: await hashPassword(password),
-		});
-
-		res.status(201).json({ message: "User created" });
-
-		client.close();
+		client = await connectToDb();
 	} catch (error) {
-		let message = "Something went wrong";
+		let message = "Something went wronga";
 		if (typeof error === "string") {
 			message = error;
 		} else if (error instanceof Error) {
 			message = error.message;
 		}
 		res.status(500).json({ message });
+		return;
 	}
+	const db = client.db();
+
+	const collection = db.collection("users");
+
+	const existingUser = await collection.findOne({ email });
+
+	if (existingUser) {
+		res.status(422).json({ message: "User already exists" });
+		client.close();
+		return;
+	}
+
+	const result = await collection.insertOne({
+		email,
+		password: await hashPassword(password),
+	});
+
+	res.status(201).json({ message: "User created" });
+
+	client.close();
 };
 
 export default handler;
