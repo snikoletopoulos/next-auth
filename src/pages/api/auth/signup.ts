77bf46e785a7507100aeeacb569e12d8a39bcfd8
Stavros <snikoletopoulos@hotmail.com>
2022-04-21 21:26:30 +0300

    Create signup api endpoint

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,9 +1,48 @@
 import type { NextApiHandler } from "next";
 
-interface Response {}
+import { connectToDb } from "helpers/db";
+import { hashPassword } from "helpers/auth";
 
-const handler: NextApiHandler = (req, res) => {
-  
+export interface Response {
+	message: string;
+}
+
+const handler: NextApiHandler<Response> = async (req, res) => {
+	if (req.method !== "POST") {
+		res.status(405).end();
+		return;
+	}
+
+	const data = req.body;
+
+	const { email, password } = data;
+
+	if (!email || !email.includes("@") || !password || password.length < 5) {
+		res.status(422).json({ message: "Invalid input" });
+		return;
+	}
+
+	const client = await connectToDb();
+	const db = client.db();
+
+	const collection = db.collection("users");
+
+	const existingUser = await collection.findOne({ email });
+
+	if (existingUser) {
+		res.status(422).json({ message: "User already exists" });
+		client.close();
+		return;
+	}
+
+	const result = await collection.insertOne({
+		email,
+		password: await hashPassword(password),
+	});
+
+	res.status(201).json({ message: "User created" });
+
+  client.close()
 };
 
 export default handler;
