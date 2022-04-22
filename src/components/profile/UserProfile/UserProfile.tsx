7016ffee25244291c9f12d472a8069d26fda34c9
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 22:20:13 +0300

    Protect profile page when user is not authenticated

--- src/components/profile/UserProfile/UserProfile.tsx
+++ src/components/profile/UserProfile/UserProfile.tsx
@@ -3,8 +3,6 @@ import styles from "./UserProfile.module.css";
 import ProfileForm from "components/profile/ProfileForm/ProfileForm";
 
 const UserProfile: React.FC = () => {
-	// Redirect away if NOT auth
-
 	return (
 		<section className={styles.profile}>
 			<h1>Your User Profile</h1>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,36 @@
-import type { NextPage } from "next";
+import type {
+	GetServerSidePropsContext,
+	NextPage,
+	InferGetServerSidePropsType,
+} from "next";
+import { getSession } from "next-auth/react";
 
 import UserProfile from "components/profile/UserProfile/UserProfile";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<
+	InferGetServerSidePropsType<typeof getServerSideProps>
+> = props => {
 	return <UserProfile />;
 };
 
 export default ProfilePage;
+
+export const getServerSideProps = async (
+	context: GetServerSidePropsContext
+) => {
+	const session = await getSession({ req: context.req });
+	console.log(session);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: "/auth",
+				permanant: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+};
