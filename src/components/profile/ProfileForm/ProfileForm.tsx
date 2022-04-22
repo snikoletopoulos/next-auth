09feee48ae11b01bfdbb0c6589657acd56ccbbc2
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 23:01:49 +0300

    Manage change password form with formik

--- src/components/profile/ProfileForm/ProfileForm.tsx
+++ src/components/profile/ProfileForm/ProfileForm.tsx
@@ -1,18 +1,42 @@
 import styles from "./ProfileForm.module.css";
 
-const ProfileForm: React.FC = () => {
+import { useFormik } from "formik";
+
+interface Props {
+	onChangePassword: (oldPassword: string, newPassword: string) => void;
+}
+
+const ProfileForm: React.FC<Props> = props => {
+	const passwordForm = useFormik({
+		initialValues: {
+			oldPassword: "",
+			newPassword: "",
+		},
+		onSubmit: async values => {
+			props.onChangePassword(values.oldPassword, values.newPassword);
+		},
+	});
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={passwordForm.handleSubmit}>
 			<div className={styles.control}>
 				<label htmlFor="new-password">New Password</label>
-				<input type="password" id="new-password" />
+				<input
+					type="password"
+					id="new-password"
+					{...passwordForm.getFieldProps("newPassword")}
+				/>
 			</div>
 			<div className={styles.control}>
 				<label htmlFor="old-password">Old Password</label>
-				<input type="password" id="old-password" />
+				<input
+					type="password"
+					id="old-password"
+					{...passwordForm.getFieldProps("oldPassword")}
+				/>
 			</div>
 			<div className={styles.action}>
-				<button>Change Password</button>
+				<button type="submit">Change Password</button>
 			</div>
 		</form>
 	);
