eeedb5390172bc71e51e339763810eb0c3ea5bba
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 22:19:52 +0300

    Protect auth page when user is authenticated

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,9 +1,37 @@
-import type { NextPage } from "next";
+import type {
+	GetServerSidePropsContext,
+	InferGetServerSidePropsType,
+	NextPage,
+	Redirect,
+} from "next";
 
 import AuthForm from "components/auth/AuthForm/AuthForm";
+import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-const AuthPage: NextPage = () => {
+const AuthPage: NextPage<
+	InferGetServerSidePropsType<typeof getServerSideProps>
+> = () => {
 	return <AuthForm />;
 };
 
 export default AuthPage;
+
+export const getServerSideProps = async (
+	context: GetServerSidePropsContext
+) => {
+	const session = await getSession({ req: context.req });
+getServerSes sion
+	if (session) {
+		return {
+			redirect: {
+				destination: "/profile",
+				permanent: false,
+			} as Redirect,
+		};
+	}
+
+	return {
+		props: {},
+	};
+};
