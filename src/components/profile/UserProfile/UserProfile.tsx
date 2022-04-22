89d96a697084a029e580a5bc5abd69425deb4826
Stavros <snikoletopoulos@hotmail.com>
2022-04-22 23:02:25 +0300

    Sent change password request

--- src/components/profile/UserProfile/UserProfile.tsx
+++ src/components/profile/UserProfile/UserProfile.tsx
@@ -1,12 +1,17 @@
 import styles from "./UserProfile.module.css";
 
 import ProfileForm from "components/profile/ProfileForm/ProfileForm";
+import { changePassword } from "services/change-password.services";
 
 const UserProfile: React.FC = () => {
+	const handlePasswordChange = (oldPassword: string, newPassword: string) => {
+		changePassword(oldPassword, newPassword);
+	};
+
 	return (
 		<section className={styles.profile}>
 			<h1>Your User Profile</h1>
-			<ProfileForm />
+			<ProfileForm onChangePassword={handlePasswordChange} />
 		</section>
 	);
 };
